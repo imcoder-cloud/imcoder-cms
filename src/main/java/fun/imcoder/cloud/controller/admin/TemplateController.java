706110030db8a2cdce90c0d5514a45b610649492
dongdongchen <dd18355193335@163.com>
2020-03-12 20:54:35 +0800

    增加功能

--- src/main/java/fun/imcoder/cloud/controller/admin/TemplateController.java
+++ src/main/java/fun/imcoder/cloud/controller/admin/TemplateController.java
@@ -10,8 +10,12 @@ import fun.imcoder.cloud.utils.FileUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/template")
@@ -46,9 +50,28 @@ public class TemplateController extends BaseController<Template, TemplateService
         return ResponseData.success(FileUtils.writeFile(file.getPath(), file.getContent()));
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("/file/delete")
     private ResponseData delete(@RequestBody ImcoderFile file) {
         return ResponseData.success(FileUtils.deleteFile(file.getPath()));
+    }
+
+    @PostMapping("/file/copy")
+    private ResponseData copyFile(@RequestBody Map<String, String> map) throws IOException {
+        String src = map.get("src");
+        String dest = map.get("dest");
+        if (StringUtils.isEmpty(dest)) {
+            dest = ImcoderConfig.TEMPLATES_DIR.replace("file:///", "") + ImcoderConfig.options.get(ImcoderConfig.OPTIONS_KEY_TEMPLATE);
+        }
+        File srcFile = FileUtils.newFile(src);
+        File destFile = FileUtils.newFile(dest);
+        if (destFile.exists()) {
+            destFile = FileUtils.newFile(destFile.getParent() + "/" + (new Date()).getTime() + "-" + destFile.getName());
+        }
+        FileUtils.copyFile(map.get("src"), dest);
+        Map<String, String> rtn = new HashMap<>();
+        rtn.put("path", destFile.getPath());
+        rtn.put("name", destFile.getName());
+        return ResponseData.success(rtn);
     }
 
 }
