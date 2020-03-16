b1b2ddd0051290acddb740d815d62fccd77e56bb
dongdongchen <dd18355193335@163.com>
2020-03-16 18:58:50 +0800

    增加功能

--- src/main/java/fun/imcoder/cloud/controller/admin/BannerController.java
+++ src/main/java/fun/imcoder/cloud/controller/admin/BannerController.java
@@ -1,12 +1,26 @@
 package fun.imcoder.cloud.controller.admin;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import fun.imcoder.cloud.base.BaseController;
+import fun.imcoder.cloud.common.ResponseData;
 import fun.imcoder.cloud.model.Banner;
 import fun.imcoder.cloud.service.BannerService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin/banner")
 public class BannerController extends BaseController<Banner, BannerService> {
+
+    @GetMapping("/group")
+    public ResponseData<List<Banner>> group() {
+        QueryWrapper<Banner> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("group_id");
+        queryWrapper.groupBy("group_id");
+        List<Banner> list = service.list(queryWrapper);
+        return ResponseData.success(list);
+    }
 
 }
--- src/main/java/fun/imcoder/cloud/service/impl/BannerServiceImpl.java
+++ src/main/java/fun/imcoder/cloud/service/impl/BannerServiceImpl.java
@@ -1,11 +1,31 @@
 package fun.imcoder.cloud.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import fun.imcoder.cloud.base.BaseServiceImpl;
 import fun.imcoder.cloud.mapper.BannerMapper;
 import fun.imcoder.cloud.model.Banner;
 import fun.imcoder.cloud.service.BannerService;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class BannerServiceImpl extends BaseServiceImpl<BannerMapper, Banner> implements BannerService {
+    @Override
+    public boolean saveBatch(Collection<Banner> entityList) {
+        entityList.forEach(o -> {
+            if (o.getGroupId() == 0) {
+                QueryWrapper<Banner> queryWrapper = new QueryWrapper<>();
+                queryWrapper.select("max(group_id) groupId");
+                Banner banner = this.baseMapper.selectOne(queryWrapper);
+                if (banner == null) {
+                    o.setGroupId(1);
+                } else {
+                    o.setGroupId(banner.getGroupId() + 1);
+                }
+                this.baseMapper.insert(o);
+            }
+        });
+        return true;
+    }
 }
